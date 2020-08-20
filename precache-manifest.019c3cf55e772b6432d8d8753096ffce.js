d161174dbe6a2373e2efddd64f6202baa9faad0e
KalinkinaD <dkalinina8@gmail.com>
2020-08-20 13:58:09 +0300

    Updates

---
+++ precache-manifest.019c3cf55e772b6432d8d8753096ffce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b42125b343195e32d61c7f29865eabc",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c44b95dbca4038386056",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a3493c08.chunk.css"
+  },
+  {
+    "revision": "69d1a3cbc2d187781052",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3792fd91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3792fd91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44b95dbca4038386056",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.994d2b1b.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
